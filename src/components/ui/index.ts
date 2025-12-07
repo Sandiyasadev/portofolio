0889851164df28a537ca628b49db3059bf71a72f
Sandiyasadev <codepansya@gmail.com>
2025-12-07 19:52:02 +0800

    mediumfix

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button.astro';
 export { default as Badge } from './Badge.astro';
 export { default as Card } from './Card.astro';
+export { default as Icon } from './Icon.astro';
